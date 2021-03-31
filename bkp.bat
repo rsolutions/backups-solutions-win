3f566caf0fdb029b6c86a302d220117ca6f1f1ad
Rafael Almeida <49452386+rsolutions@users.noreply.github.com>
2021-03-31 10:38:48 -0300

    Add files via upload

---
+++ bkp.bat
@@ -0,0 +1,34 @@
+﻿@echo off
+@echo ::::rALMEIDA - Soluções em TI::::
+@echo Suporte: 62 9 9942-4108
+
+set cliente=Almeida Consultoria
+set data=%date:~0,2%-%date:~3,2%-%date:~6,10%_%time:~0,2%-%time:~3,2%
+set dir_postgres=C:\Program Files\PostgreSQL\9.6\bin
+set dir_bkp=C:\GoogleDrive\Bkp_AlterData
+set bd=ALTERDATA
+
+
+REM IP do Servidor do PostgreSQL:
+SET PGHOST=localhost
+
+REM Porta de acesso ao PostgreSQL:
+SET PGPORT=5432
+
+REM Base de Dados que será feito backup:
+SET PGDATABASE=ALTERDATA
+
+REM Usuário da base de dados:
+SET PGUSER=postgres
+
+
+REM Senha da base de dados
+SET PGPASSWORD=#abc123#
+
+echo "Backup banco de dados %cliente%"
+chdir %dir_postgres%
+echo "Aguarde, realizando o backup do Banco de Dados..."
+pg_dump --host localhost --port 5432 --username "postgres" --format custom --blobs -Z4 -Fc --verbose --file "%dir_bkp%\%data%.backup" "ALTERDATA"
+echo "Backup realizado com sucesso!"
+
+exit
---
+++ deleta.bat
@@ -0,0 +1,29 @@
+﻿@echo off
+
+echo ****************************************
+
+echo * rALMEIDA - Soluções em TI  *
+echo * Deleta arquivos antigos         *
+
+echo ****************************************
+
+echo ****************************************
+
+echo * Suporte - 62 9 9942-4108             *
+echo * Email - rafaah.psy@gmail.com        *
+
+echo ****************************************
+
+cd "C:\GoogleDrive\Bkp_AlterData\"
+
+echo ****************************************
+
+echo * Excluindo arquivos antigos de backup *
+
+echo ****************************************
+
+FORFILES /D -05 /M *.backup /c "cmd /c del @file | echo @file foi excluido"
+
+
+
+EXIT
